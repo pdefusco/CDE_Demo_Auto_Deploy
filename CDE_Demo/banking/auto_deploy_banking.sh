08fdf70cc8576b6a5306ce48ac9aec907d178ed1
pdefusco <pauldefusco@gmail.com>
2023-10-26 18:19:05 -0700

    added airflow params

--- CDE_Demo/banking/auto_deploy_banking.sh
+++ CDE_Demo/banking/auto_deploy_banking.sh
@@ -13,8 +13,8 @@ echo "Provided CDE User: "$cde_user
 #CREATE DOCKER RUNTIME RESOURCE
 echo "Create CDE Credential docker-creds-"$cde_user"-banking"
 cde credential create --name docker-creds-$cde_user"-banking" --type docker-basic --docker-server hub.docker.com --docker-username $docker_user
-echo "Create CDE Docker Runtime dex-spark-runtime-great-expectations-data-quality-"$cde_user"-banking"
-cde resource create --name dex-spark-runtime-great-expectations-data-quality-$cde_user"-banking" --image pauldefusco/dex-spark-runtime-3.2.3-7.2.15.8:1.20.0-b15-great-expectations-data-quality --image-engine spark3 --type custom-runtime-image
+echo "Create CDE Docker Runtime dex-spark-runtime-ge-data-quality-"$cde_user"-banking"
+cde resource create --name dex-spark-runtime-ge-data-quality-$cde_user"-banking" --image pauldefusco/dex-spark-runtime-3.2.3-7.2.15.8:1.20.0-b15-great-expectations-data-quality --image-engine spark3 --type custom-runtime-image
 
 # CREATE FILE RESOURCE
 echo "Create Resource cde_demo_files-"$cde_user"-banking"
@@ -29,13 +29,13 @@ echo "Upload batch_load.py to cde_demo_files-"$cde_user"-banking"
 cde resource upload --name cde_demo_files-$cde_user"-banking" --local-path CDE_Demo/banking/spark/batch_load.py
 echo "Upload ge_data_quality.py to cde_demo_files-"$cde_user"-banking"
 cde resource upload --name cde_demo_files-$cde_user"-banking" --local-path CDE_Demo/banking/spark/ge_data_quality.py
-echo "Upload batch_load.py to cde_demo_files-"$cde_user
-cde resource upload --name cde_demo_files-$cde_user"-banking" --local-path CDE_Demo/banking/spark/data_quality_orchestration.py
+echo "Upload airflow.py to cde_demo_files-"$cde_user
+cde resource upload --name cde_demo_files-$cde_user"-banking" --local-path CDE_Demo/banking/airflow/airflow.py
 
 echo "Create job batch_load-"$cde_user"-banking"
-cde job create --name batch_load-$cde_user"-banking" --type spark --mount-1-prefix jobCode/ --mount-1-resource cde_demo_files-$cde_user"-banking" --application-file batch_load.py --runtime-image-resource-name dex-spark-runtime-great-expectations-data-quality-$cde_user"-banking"
+cde job create --name batch_load-$cde_user"-banking" --type spark --mount-1-prefix jobCode/ --mount-1-resource cde_demo_files-$cde_user"-banking" --application-file batch_load.py --runtime-image-resource-name dex-spark-runtime-ge-data-quality-$cde_user"-banking"
 echo "Create job ge_data_quality-"$cde_user"-banking"
-cde job create --name ge_data_quality-$cde_user"-banking" --type spark --mount-1-resource cde_demo_files-$cde_user"-banking" --application-file ge_data_quality.py --runtime-image-resource-name dex-spark-runtime-great-expectations-data-quality-$cde_user"-banking"
+cde job create --name ge_data_quality-$cde_user"-banking" --type spark --mount-1-resource cde_demo_files-$cde_user"-banking" --application-file ge_data_quality.py --runtime-image-resource-name dex-spark-runtime-ge-data-quality-$cde_user"-banking"
 echo "Create job data_quality_orchestration-"$cde_user"-banking"
 cde job create --name data_quality_orchestration-$cde_user"-banking" --type airflow --mount-1-resource cde_demo_files-$cde_user"-banking" --dag-file airflow.py
 
--- CDE_Demo/banking/auto_destroy_banking.sh
+++ CDE_Demo/banking/auto_destroy_banking.sh
@@ -8,23 +8,25 @@ echo ".."
 echo "."
 echo "Provided CDE User: "$cde_user
 
-cde credential delete --name docker-creds-$cde_user
+echo "Delete CDE Credentials for User: "$cde_user
+cde credential delete --name docker-creds-$cde_user"-banking"
 echo "Delete Jobs"
 echo "Delete batch_load-"$cde_user"-banking"
 cde job delete --name batch_load-$cde_user"-banking"
 echo "Delete ge_data_quality-"$cde_user"-banking"
-cde job delete --name create_staging_table-$cde_user"-banking"
+cde job delete --name ge_data_quality-$cde_user"-banking"
 echo "Delete data_quality_orchestration-"$cde_user"-banking"
 cde job delete --name data_quality_orchestration-$cde_user"-banking"
 
 echo "Delete resosurce dex-spark-runtime-great-expectations-data-quality-"$cde_user"-banking"
-cde resource delete --name dex-spark-runtime-great-expectations-data-quality-$cde_user"-banking"
-echo "Upload cleanup script to resource cde_demo_files-"$cde_user
+cde resource delete --name dex-spark-runtime-ge-data-quality-$cde_user"-banking"
+echo "Upload cleanup script to resource cde_demo_files-"$cde_user"-banking"
 cde resource upload --name cde_demo_files-$cde_user"-banking" --local-path CDE_Demo/banking/spark/cleanup.py
 echo "Create cleanup job cleanup-"$cde_user"-banking"
 cde job create --name cleanup-$cde_user"-banking" --type spark --mount-1-resource cde_demo_files-$cde_user"-banking" --application-file cleanup.py
 echo "Run cleanup job cleanup-"$cde_user"-banking"
 cde job run --name cleanup-$cde_user"-banking"
+
 n=1
 while [ $n -lt 50 ]
 do
@@ -33,6 +35,7 @@ do
   echo " "
   ((n=$n+1))
 done
+
 echo "Delete cleanup job cleanup-"$cde_user"-banking"
 cde job delete --name cleanup-$cde_user"-banking"
 echo "Delete resource cde_demo_files-"$cde_user"-banking"
