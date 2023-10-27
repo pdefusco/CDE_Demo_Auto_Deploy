75e8f752eef57d7fe38224c7221be3244d1e5bab
pdefusco <pauldefusco@gmail.com>
2023-10-26 20:10:25 -0700

    added airflow orch to telco demo

---
+++ CDE_Demo/telco/airflow/airflow.py
@@ -0,0 +1,93 @@
+#****************************************************************************
+# (C) Cloudera, Inc. 2020-2022
+#  All rights reserved.
+#
+#  Applicable Open Source License: GNU Affero General Public License v3.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# #  Author(s): Paul de Fusco
+#***************************************************************************/
+
+# Airflow DAG
+from datetime import datetime, timedelta
+from dateutil import parser
+from airflow import DAG
+from cloudera.cdp.airflow.operators.cde_operator import CDEJobRunOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash import BashOperator
+from airflow.models.param import Param
+
+username = "pauldefusco" # Enter your workload username here
+cde_demo = "telco"
+dag_name = "CDE_Demo_"+username+"-"+cde_demo
+
+print("Using DAG Name: {}".format(dag_name))
+
+default_args = {
+        'owner':username,
+        'start_date': datetime(2023,1,1,1),
+        'depends_on_past': False,
+        'retries':1,
+        'schedule_interval':'*/5 * * * *', #timedelta(minutes=5)
+        'retry_delay': timedelta(minutes=15),
+        'end_date': datetime(2025,1,1,1)
+        }
+
+airflow_dag = DAG(
+        dag_name,
+        default_args=default_args,
+        catchup=False,
+        schedule_interval='*/5 * * * *',
+        is_paused_upon_creation=False
+        )
+
+start = DummyOperator(
+        task_id="start",
+        dag=airflow_dag
+)
+
+batch_load_step = CDEJobRunOperator(
+        task_id='geospatial-rdd-telco',
+        dag=airflow_dag,
+        job_name='geospatial_rdd-'+username+"-"+cde_demo, #Must match name of CDE Spark Job in the CDE Jobs UI
+        trigger_rule='all_success',
+        )
+
+data_quality_step = CDEJobRunOperator(
+        task_id='geospatial-joins-telco',
+        dag=airflow_dag,
+        job_name='geospatial_joins-'+username+"-"+cde_demo, #Must match name of CDE Spark Job in the CDE Jobs UI
+        trigger_rule='all_success',
+        )
+
+start >> batch_load_step >> data_quality_step
--- CDE_Demo/telco/auto_deploy_telco.sh
+++ CDE_Demo/telco/auto_deploy_telco.sh
@@ -24,7 +24,7 @@ cde resource upload-archive --name countries_data-$cde_user"-telco" --local-path
 echo "Create CDE Resource for Job Dependencies"
 cde resource create --name job_code-$cde_user"-telco"
 echo "Upload Job Dependencies to CDE Files Resource"
-cde resource upload --name job_code-$cde_user"-telco" --local-path CDE_Demo/telco/spark/geospatial_joins.py --local-path CDE_Demo/telco/spark/geospatial_rdd.py --local-path CDE_Demo/telco/spark/parameters.conf --local-path CDE_Demo/telco/spark/utils.py
+cde resource upload --name job_code-$cde_user"-telco" --local-path CDE_Demo/telco/spark/geospatial_joins.py --local-path CDE_Demo/telco/spark/geospatial_rdd.py --local-path CDE_Demo/telco/spark/parameters.conf --local-path CDE_Demo/telco/spark/utils.py --local-path CDE_Demo/telco/airflow/airflow.py
 
 #current_date_time=$(date +"%Y-%m-%d %T" )
 
@@ -32,24 +32,19 @@ cde resource upload --name job_code-$cde_user"-telco" --local-path CDE_Demo/telc
 echo "Create GEOSPATIAL RDD SPARK JOB"
 cde job create --name geospatial_rdd-$cde_user"-telco" --type spark --mount-1-prefix jobCode/ --mount-1-resource job_code-$cde_user"-telco" --mount-2-prefix countriesData/ --mount-2-resource countries_data-$cde_user"-telco" --runtime-image-resource-name dex-spark-runtime-sedona-geospatial-$cde_user"-telco" --packages org.apache.sedona:sedona-spark-shaded-3.0_2.12:1.5.0,org.datasyslab:geotools-wrapper:1.5.0-28.2 --application-file jobCode/geospatial_rdd.py
 
-echo "Run GEOSPATIAL RDD SPARK JOB"
-cde job run --name geospatial_rdd-$cde_user"-telco" --executor-cores 2 --executor-memory "4g"
+#echo "Run GEOSPATIAL RDD SPARK JOB"
+#cde job run --name geospatial_rdd-$cde_user"-telco" --executor-cores 2 --executor-memory "4g"
 
-n=1
-while [ $n -lt 10 ]
-do
-  echo "Running GEOSPATIAL RDD Job..."
-  sleep 2
-  echo " "
-  ((n=$n+1))
-done
 
-# RUN CDE JOBS
-echo "Run GEOSPATIAL RDD SPARK JOB"
-cde job run --name geospatial_rdd-$cde_user"-telco" --executor-cores 2 --executor-memory "4g"
+# CREATE CDE JOBS
+echo "Create GEOSPATIAL JOINS SPARK JOB"
+cde job create --name geospatial_joins-$cde_user"-telco" --type spark --mount-1-prefix jobCode/ --mount-1-resource job_code-$cde_user"-telco" --mount-2-prefix countriesData/ --mount-2-resource countries_data-$cde_user"-telco" --runtime-image-resource-name dex-spark-runtime-sedona-geospatial-$cde_user"-telco" --packages org.apache.sedona:sedona-spark-shaded-3.0_2.12:1.5.0,org.datasyslab:geotools-wrapper:1.5.0-28.2 --application-file jobCode/geospatial_joins.py
 
-echo "RUN GEOSPATIAL JOINS SPARK JOB"
-cde job run --name geospatial_joins-$cde_user"-telco" --executor-cores 2 --executor-memory "4g"
+#echo "RUN GEOSPATIAL JOINS SPARK JOB"
+#cde job run --name geospatial_joins-$cde_user"-telco" --executor-cores 2 --executor-memory "4g"
+echo "Create GEOSPATIAL AIRFLOW ORCHESTRATION JOB"
+cde job create --name geo_orch-$cde_user"-telco" --type airflow --mount-1-prefix jobCode/ --mount-1-resource job_code-$cde_user"-telco" --dag-file airflow.py
+
 echo " "
 echo "."
 echo ".."
